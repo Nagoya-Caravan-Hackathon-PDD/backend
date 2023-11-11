54580a3a6f472c2a5bd094c8189d4e1f4847fee9
murasame <oogiriminister@gmail.com>
2023-11-11 16:55:50 +0900

    add:new router

--- src/infrastructure/http/router/router.go
+++ src/infrastructure/http/router/router.go
@@ -1,3 +1,17 @@
 package router
 
+import "net/http"
+
 // TODO: ルーティングを定義する
+
+type router struct {
+	Mux *http.ServeMux
+}
+
+func NewRouter() *router {
+	router := &router{
+		Mux: http.NewServeMux(),
+	}
+
+	return router
+}
