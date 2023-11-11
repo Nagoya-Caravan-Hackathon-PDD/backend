457205c9c450d13e47d48592ce8d99135d7e3c42
murasame <oogiriminister@gmail.com>
2023-11-11 16:57:20 +0900

    add: run server

--- src/infrastructure/server/server.go
+++ src/infrastructure/server/server.go
@@ -1,3 +1,16 @@
 package server
 
+import "github.com/Nagoya-Caravan-Hackathon-PDD/backend/src/infrastructure/http/router"
+
 // TODO: サーバの起動と停止を行う
+type httpServer struct {
+}
+
+func NewHTTPserver() *httpServer {
+	return &httpServer{}
+}
+
+func (s *httpServer) Run() {
+	router := router.NewRouter()
+	runWithGracefulShutdown(router.Mux)
+}
