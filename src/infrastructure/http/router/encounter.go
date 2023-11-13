3f28ac38da14e197f2a29b3cb24868ce1645f2d5
murasame <oogiriminister@gmail.com>
2023-11-14 02:35:10 +0900

    add:new route

--- src/infrastructure/http/router/encounter.go
+++ src/infrastructure/http/router/encounter.go
@@ -1,10 +1,21 @@
 package router
 
-import "github.com/Nagoya-Caravan-Hackathon-PDD/backend/src/adapters/controllers"
+import (
+	"github.com/Nagoya-Caravan-Hackathon-PDD/backend/src/adapters/controllers"
+	"github.com/Nagoya-Caravan-Hackathon-PDD/backend/src/adapters/gateways"
+	"github.com/Nagoya-Caravan-Hackathon-PDD/backend/src/adapters/presenters"
+	"github.com/Nagoya-Caravan-Hackathon-PDD/backend/src/usecase/interactors"
+)
 
 func (r *router) encounterRoutes() {
 	g := r.echo.Group("/v1")
-	ec := controllers.NewEncounterController()
+	ec := controllers.NewEncounterController(
+		interactors.NewEncounterInteracter(
+			gateways.NewEncounterGateway(r.db),
+			presenters.NewEncounterPresenter(),
+		),
+	)
 
 	g.POST("/encounters", ec.Create)
+	g.GET("/encounters", ec.List)
 }
