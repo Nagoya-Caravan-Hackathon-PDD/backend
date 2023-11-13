6d1c2805e4476c02f505dd06539abe36797b8466
murasame <oogiriminister@gmail.com>
2023-11-14 02:35:03 +0900

    add:controllers

--- src/adapters/controllers/encounter_controller.go
+++ src/adapters/controllers/encounter_controller.go
@@ -10,8 +10,10 @@ type encounterController struct {
 	interactor ports.EncounterInput
 }
 
-func NewEncounterController() *encounterController {
-	return &encounterController{}
+func NewEncounterController(interactor ports.EncounterInput) *encounterController {
+	return &encounterController{
+		interactor: interactor,
+	}
 }
 
 func (ec *encounterController) Create(ctx echo.Context) error {
@@ -21,4 +23,12 @@ func (ec *encounterController) Create(ctx echo.Context) error {
 	}
 
 	return ctx.JSON(ec.interactor.Create(reqBody))
+}
+
+func (ec *encounterController) List(ctx echo.Context) error {
+	var reqBody input.ListEncounterRequest
+	if err := ctx.Bind(&reqBody); err != nil {
+		return echo.ErrBadRequest
+	}
+	return ctx.JSON(ec.interactor.List(reqBody))
 }
