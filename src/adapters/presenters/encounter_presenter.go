6e2261b6377fe6137a87327fe902cf1bafcecc9b
murasame <oogiriminister@gmail.com>
2023-11-14 02:30:27 +0900

    add: Read All presenters

--- src/adapters/presenters/encounter_presenter.go
+++ src/adapters/presenters/encounter_presenter.go
@@ -1,10 +1,12 @@
 package presenters
 
 import (
+	"log"
 	"net/http"
 	"reflect"
 
 	"github.com/Nagoya-Caravan-Hackathon-PDD/backend/src/datastructure/output"
+	"github.com/Nagoya-Caravan-Hackathon-PDD/backend/src/datastructure/types"
 	"github.com/labstack/echo/v4"
 	"github.com/lib/pq"
 )
@@ -16,19 +18,50 @@ func NewEncounterPresenter() *encounterPresenter {
 	return &encounterPresenter{}
 }
 
-func (p *encounterPresenter) CreateEncounterResponse(err error) (int, *output.CreateEncounterResponse) {
+func (p *encounterPresenter) CreateEncounterResponse(encounterID string, err error) (int, *output.CreateEncounterResponse) {
+
 	if err != nil {
 		switch reflect.TypeOf(err) {
 		case reflect.TypeOf(&pq.Error{}):
-			return http.StatusBadRequest, &output.CreateEncounterResponse{}
+			return http.StatusBadRequest, nil
 		case reflect.TypeOf(echo.ErrBadRequest):
-			return http.StatusBadRequest, &output.CreateEncounterResponse{}
+			return http.StatusBadRequest, nil
 		case reflect.TypeOf(echo.ErrInternalServerError):
-			return http.StatusInternalServerError, &output.CreateEncounterResponse{}
+			return http.StatusInternalServerError, nil
 		default:
-			return http.StatusInternalServerError, &output.CreateEncounterResponse{}
+			return http.StatusInternalServerError, nil
 		}
 	}
 
-	return http.StatusOK, &output.CreateEncounterResponse{}
+	return http.StatusOK, &output.CreateEncounterResponse{
+		EncounterID: encounterID,
+	}
+}
+
+func (p *encounterPresenter) GetEncounterResponse(args []types.ReadEncounter, err error) (int, []*output.ListEncounterResponse) {
+	log.Println(args, err)
+	if err != nil {
+		log.Println(err)
+		switch reflect.TypeOf(err) {
+		case reflect.TypeOf(&pq.Error{}):
+			return http.StatusBadRequest, nil
+		case reflect.TypeOf(echo.ErrBadRequest):
+			return http.StatusBadRequest, nil
+		case reflect.TypeOf(echo.ErrInternalServerError):
+			return http.StatusInternalServerError, nil
+		default:
+			return http.StatusInternalServerError, nil
+		}
+	}
+
+	var res []*output.ListEncounterResponse
+	for _, arg := range args {
+		res = append(res, &output.ListEncounterResponse{
+			EncounterID:    arg.EncounterID,
+			UserID:         arg.UserID,
+			EncoutedUserID: arg.EncountedUserID,
+			CreatedAt:      arg.CreatedAt,
+		})
+	}
+	return http.StatusOK, res
 }
