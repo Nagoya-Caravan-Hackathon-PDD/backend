f5456925223f967116c10eb009afd2f1c93a6bf6
murasame <oogiriminister@gmail.com>
2023-11-14 02:33:47 +0900

    add: readAll interface

--- src/usecase/ports/encounter_port.go
+++ src/usecase/ports/encounter_port.go
@@ -3,12 +3,15 @@ package ports
 import (
 	"github.com/Nagoya-Caravan-Hackathon-PDD/backend/src/datastructure/input"
 	"github.com/Nagoya-Caravan-Hackathon-PDD/backend/src/datastructure/output"
+	"github.com/Nagoya-Caravan-Hackathon-PDD/backend/src/datastructure/types"
 )
 
 type EncounterInput interface {
 	Create(arg input.CreateEncounterReqeuest) (int, *output.CreateEncounterResponse)
+	List(args input.ListEncounterRequest) (int, []*output.ListEncounterResponse)
 }
 
 type EncounterOutput interface {
 	CreateEncounterResponse(encounterID string, err error) (int, *output.CreateEncounterResponse)
+	GetEncounterResponse(args []types.ReadEncounter, err error) (int, []*output.ListEncounterResponse)
 }
