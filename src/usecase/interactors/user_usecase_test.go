dcaf2591634e6f6a9a26b217d4252125cc9a59fe
murasame <oogiriminister@gmail.com>
2023-11-15 11:45:31 +0900

    add: read usecase test

--- src/usecase/interactors/user_usecase_test.go
+++ src/usecase/interactors/user_usecase_test.go
@@ -83,7 +83,58 @@ func TestCreate(t *testing.T) {
 			}
 		})
 	}
+	testGetUser(t)
 	testDelete(t)
+}
+
+func testGetUser(t *testing.T) {
+	ui := NewUserInteractor(gateways.NewUserGateway(dbconn), presenters.NewUserPresenter())
+	testCases := []struct {
+		name       string
+		arg        input.GetUser
+		wantStatus int
+		wantBody   *output.ReadUserResponse
+	}{
+		{
+			name: "success",
+			arg: input.GetUser{
+				UserID: "test_user_id",
+			},
+			wantStatus: http.StatusOK,
+			wantBody: &output.ReadUserResponse{
+				UserID:   "test_user_id",
+				GitHubID: "test_github_id",
+			},
+		},
+		{
+			name: "bad request uid empty",
+			arg: input.GetUser{
+				UserID: "",
+			},
+			wantStatus: http.StatusBadRequest,
+			wantBody:   nil,
+		},
+		{
+			name: "bad request uid not found",
+			arg: input.GetUser{
+				UserID: "test_user_id_not_found",
+			},
+			wantStatus: http.StatusNotFound,
+			wantBody:   nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			status, body := ui.Read(tc.arg)
+			if status != tc.wantStatus {
+				t.Errorf("got %v, want %v", status, tc.wantStatus)
+			}
+			if reflect.TypeOf(body) != reflect.TypeOf(tc.wantBody) {
+				t.Errorf("got %v, want %v", body, tc.wantBody)
+			}
+		})
+	}
 }
 
 func testDelete(t *testing.T) {
