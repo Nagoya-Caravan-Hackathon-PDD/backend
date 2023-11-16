ad89da1b96f67d64e91719e6c4bd7e14cdbffbb6
murasame <oogiriminister@gmail.com>
2023-11-17 03:06:57 +0900

    add:new schema

---
+++ cmd/migration/000005_gametemp.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "game_temp" (
+  "game_id" varchar NOT NULL,
+  "owner_id" varchar NOT NULL,
+  "owner_is_ok" bool NOT NULL,
+  "enemy_id" varchar,
+  "enemy_is_ok" bool NOT NULL,
+  "is_block" bool NOT NULL
+);
