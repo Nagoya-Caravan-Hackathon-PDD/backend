cdf0ec4e1e95bc4236c6996aa3537a0955e8e92f
murasame29 <oogiriminister@gmail.com>
2023-11-16 14:36:21 +0900

    add: game route

--- src/infrastructure/server/server.go
+++ src/infrastructure/server/server.go
@@ -3,19 +3,21 @@ package server
 import (
 	"database/sql"
 
+	firebase "firebase.google.com/go"
 	"github.com/Nagoya-Caravan-Hackathon-PDD/backend/src/infrastructure/http/router"
 )
 
 // TODO: サーバの起動と停止を行う
 type httpServer struct {
-	db *sql.DB
+	db  *sql.DB
+	app *firebase.App
 }
 
-func NewHTTPserver(db *sql.DB) *httpServer {
-	return &httpServer{db}
+func NewHTTPserver(db *sql.DB, app *firebase.App) *httpServer {
+	return &httpServer{db, app}
 }
 
 func (s *httpServer) Run() {
-	serv := router.NewRouter(s.db)
+	serv := router.NewRouter(s.db, s.app)
 	runWithGracefulShutdown(serv)
 }
