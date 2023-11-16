989205224ce1336e85a6d813876d94c23bb947c9
murasame <oogiriminister@gmail.com>
2023-11-17 03:09:42 +0900

    add: ingame endpoints

---
+++ src/infrastructure/http/router/ingame.go
@@ -0,0 +1,31 @@
+package router
+
+import (
+	"github.com/Nagoya-Caravan-Hackathon-PDD/backend/cmd/config"
+	"github.com/Nagoya-Caravan-Hackathon-PDD/backend/pkg/paseto"
+	"github.com/Nagoya-Caravan-Hackathon-PDD/backend/src/adapters/controllers"
+	"github.com/Nagoya-Caravan-Hackathon-PDD/backend/src/adapters/gateways"
+	"github.com/Nagoya-Caravan-Hackathon-PDD/backend/src/adapters/presenters"
+	"github.com/Nagoya-Caravan-Hackathon-PDD/backend/src/usecase/interactors"
+)
+
+func (r *router) ingameRouter() {
+	maker, err := paseto.NewPasetoMaker(config.Config.Paseto.SecretKey)
+	if err != nil {
+		panic(err)
+	}
+	gc := controllers.NewGameController(
+		interactors.NewGameInteractor(
+			gateways.NewFireStoreGateway(r.app),
+			gateways.NewGitmonGateway(r.db),
+			maker,
+			presenters.NewGamePresenter(),
+		),
+	)
+
+	g := r.echo.Group("/v1")
+
+	g.POST("/game/:game_id/ready", gc.Ready)
+	g.POST("/game/:game_id/action", gc.Action)
+	g.POST("/game/:game_id/fin_turn", gc.FinTurn)
+}
